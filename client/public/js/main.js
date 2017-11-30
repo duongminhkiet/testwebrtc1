b910ebea603ddfb5d836f093566b7f7bbc2b51ff
Minh Kiet <kietdk@gmail.com>
2017-11-30 23:13:20 +0700

    set audio -> true

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -20,7 +20,7 @@ socket.on('DANH_SACH_ONLINE', arrUserInfo => {
 socket.on('DANG_KY_THAT_BAI', () => alert('Ten nay da ton tai! Vui long chon ten khac'));
 
 function openStream(){
-	const config = {audio: false, video:true};
+	const config = {audio: true, video:true};
 	return navigator.mediaDevices.getUserMedia(config);
 }
 function playStream(idVideoTag, stream){
